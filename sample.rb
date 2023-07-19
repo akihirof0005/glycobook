3509de25e2bf4bd5c883a0c414be2f36070c68a2
akihiro fujita <akihirof0005@gmail.com>
2023-07-20 00:08:46 +0900

    for merge

--- sample.rb
+++ sample.rb
@@ -1,8 +1,16 @@
 require 'wurcsverify'
 require 'csv'
+#require 'parallel'
 
 data_path = './glytoucan-glycan.csv'
 csv_data = CSV.read(data_path, encoding: 'bom|utf-8')
+
+kawari_path = './archetype.csv'
+kawari_data = CSV.read(kawari_path, encoding: 'bom|utf-8')
+kawari={}
+kawari_data.each do |item|
+ kawari[item[0]] = item[1]
+end
 
 def aligned_character_color_diff(one, other)
     max_length = [one.length, other.length].max
@@ -26,10 +34,15 @@ def aligned_character_color_diff(one, other)
     return [ flag, one_diff, other_diff]
 end
 count =0
-puts "|ID|version|sequence|"
-puts "| :--- | :--- | :--- |"
 wf = WurcsVerify.initialize_module
+error = "|ID|version|sequence|\n| :--- | :--- | :--- |\n"
+unverifiable =  "|ID|version|sequence|\n| :--- | :--- | :--- |\n"
+diff =  "|ID|version|sequence|\n| :--- | :--- | :--- |\n"
+#Parallel.each(csv_data,in_threads: 15 ) do |id,w|
 csv_data.each do |id,w|
+if kawari.key?(w)
+    w = kawari[w]
+  end
 #csv_data[169089..170000].each do |id,w|
   count = count +1
   $stderr.print "\r"+count.to_s
@@ -40,24 +53,27 @@ csv_data.each do |id,w|
   if wfvv["latest"]["ERROR"]
     if wfvv["1.0.1"]["ERROR"]
       $stderr.puts id
-      puts "|"+id+"|1.0.1| " + wfvv["1.0.1"]["RESULTS"].gsub(/\R/, "<br>") + "|"
-      puts "|^ |latest| " + wfvv["latest"]["RESULTS"].gsub(/\R/, "<br>") + "|"
+      error = error +"\n"+ "|"+id+"|1.0.1| " + wfvv["1.0.1"]["RESULTS"].gsub(/\R/, "<br>") + "|"
+      error = error +"\n"+ "|"+id+"|latest| " + wfvv["latest"]["RESULTS"].gsub(/\R/, "<br>") + "|"
     else
-      puts "|"+id+"|1.0.1| " + wfvv["1.0.1"]["STANDERD"] + "|"
-      puts "|^ |latest| " + wfvv["latest"]["RESULTS"].gsub(/\R/, "<br>") + "|"
+      error = error +"\n"+ "|"+id+"|1.0.1| " + wfvv["1.0.1"]["STANDERD"] + "|"
+      error = error +"\n"+ "|"+id+"|latest| " + wfvv["latest"]["RESULTS"].gsub(/\R/, "<br>") + "|"
     end
   elsif wfvv["latest"]["UNVERIFIABLE"]
-    puts "|"+id+"|1.0.1| " + wfvv["1.0.1"]["STANDERD"] + "|"
-    puts "|^ |latest| " + wfvv["latest"]["RESULTS"].gsub(/\R/, "<br>")+ "|"
+    unverifiable = unverifiable +"\n"+ "|"+id+"|1.0.1| " + wfvv["1.0.1"]["STANDERD"] + "|"
+    unverifiable = unverifiable +"\n"+ "|"+id+"|latest| " + wfvv["latest"]["RESULTS"].gsub(/\R/, "<br>")+ "|"
   else
     results = aligned_character_color_diff(
       wfvv["1.0.1"]["STANDERD"],
       wfvv["latest"]["STANDERD"])
     if results[0]
-      puts "|"+id+"|1.0.1| " + results[1] + "|"
-      puts "|^ |latest| " + results[2] + "|"
+      diff = diff + "\n"+"|"+id+"|1.0.1| " + results[1] + "|"
+      diff = diff +"\n"+"|"+id+"|latest| " + results[2] + "|"
     else
       next
     end
   end
 end
+File.write("error.md", error)
+File.write("unverifiable.md", unverifiable)
+File.write("diff.md", diff)
---
+++ test.rb
@@ -0,0 +1,14 @@
+require_relative './lib/glycanformatconverter.rb'
+
+
+w = "WURCS=2.0/3,5,4/[a2122h-1b_1-5_2*NCC/3=O][a1122h-1b_1-5][a1122h-1a_1-5]/1-1-2-3-3/a4-b1_b4-c1_c3-d1_c6-e1"
+
+iupac = GlycanFormatConverter.wurcs2iupac(w,"condensed")
+iupacex = GlycanFormatConverter.wurcs2iupac(w,"extended")
+ct = GlycanFormatConverter.wurcs2glycoct(w)
+glycam = GlycanFormatConverter.wurcs2glycam(w)
+
+puts iupac
+puts iupacex
+puts ct
+puts glycam
