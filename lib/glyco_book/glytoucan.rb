9f52534c404f81eb93cecd4718ab3b0ec25aac32
akihiro fujita <akihirof0005@gmail.com>
2023-07-23 17:27:36 +0900

    update namespace

---
+++ lib/glyco_book/glytoucan.rb
@@ -0,0 +1,18 @@
+require 'java'
+
+require_relative 'jar/archetype.jar'
+require_relative 'jar/wurcsframework.jar'
+require_relative 'jar/slf4j-api.jar'
+
+java_import 'org.glytoucan.Archetype'
+java_import 'org.glycoinfo.WURCSFramework.wurcs.graph.WURCSGraph'
+java_import 'org.glycoinfo.WURCSFramework.util.WURCSException'
+java_import 'org.slf4j.Logger'
+java_import 'org.slf4j.LoggerFactory'
+module GlucoBook
+class GlyTouCan
+  def archetype(w)
+    Hash[Archetype.beBorn(w)]
+  end
+end
+end
--- lib/glycobook.rb
+++
@@ -1,7 +0,0 @@
-module Glycobook
-  load 'glycanbuilder.rb'
-  load 'wurcsframework.rb'
-  load 'glycanformatconverter.rb'
-  load 'glytoucan.rb'
-  load 'subsumption.rb'
-end
