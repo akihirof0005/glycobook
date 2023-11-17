b9db61d03654dd95ebbc48acceb75ee2c9c56027
akihiro <akihirof0005@gmail.com>
2023-11-17 12:16:36 +0900

    update

--- lib/glycobook/Loggerinit.rb
+++ lib/glycobook/Loggerinit.rb
@@ -1,23 +1,31 @@
 require 'java'
 require 'logger'
 require_relative  '../jar/slf4j-api.jar'
+require_relative  '../jar/slf4j-simple.jar'
+
 java_import 'org.slf4j.LoggerFactory'
 
 class JRubySLF4JLogger < Logger
   def initialize(name)
-    super(STDOUT)  # STDOUTはダミー、実際のログはSLF4Jを通じて行われる
+    super(STDOUT)  # STDOUT is dummy
     @java_logger = LoggerFactory.getLogger(name)
   end
+  def add(severity, message , progname , &block)
 
-  def add(severity, message = nil, progname = nil)
-    # Rubyのログレベルに応じてJavaのSLF4Jメソッドを呼び出す
-    case severity
-    when Logger::INFO
+  message = block.call if block
+  message = message.to_s
+
+  case severity
+    when 0  # Logger::DEBUG
+      @java_logger.debug(message)
+    when 1  # Logger::INFO
       @java_logger.info(message)
-    # 他のログレベルについても同様に実装
+    when 2  # Logger::WARN
+      @java_logger.warn(message)
+    when 3, 4# Logger::ERROR, Logger::FATAL
+      @java_logger.error(message)
+    else
+      @java_logger.info(message)
     end
   end
 end
-
-logger = JRubySLF4JLogger.new("MyLogger")
-logger.info("This is an info message from JRuby with SLF4J")
