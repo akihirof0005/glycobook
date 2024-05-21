ad3cd8e3f5ef27bc45a2375b459f15d748a6f387
akihiro fujita <akihirof0005@gmail.com>
2024-05-21 15:38:16 +0900

    update

--- lib/wurcsverify.rb
+++ lib/wurcsverify.rb
@@ -10,11 +10,24 @@ java_import 'java.net.URLClassLoader'
 java_import 'org.slf4j.Logger'
 
 class WurcsVerify
+
+  @@gtc_version = "1.2.14"
+  @@v1215 = "1.2.15"
+  @@v1216 = "1.2.16"
+  @@v130 = "1.3.0"
+  @@v131 = "1.3.1"
+
   def initialize
-      @wfw_glytoucan_loader = self.create_custom_classloader("jar/wurcsframework-1.2.14.jar")
-      @wfw_131_loader = self.create_custom_classloader("jar/wurcsframework-1.3.1.jar")
+      @wfw_glytoucan_loader = self.create_custom_classloader("jar/wurcsframework-" + @@gtc_version + ".jar")
+      @wfw_v131_loader = self.create_custom_classloader("jar/wurcsframework-" + @@v131 + ".jar")
+      @wfw_v130_loader = self.create_custom_classloader("jar/wurcsframework-" + @@v130 + ".jar")
+      @wfw_v1216_loader = self.create_custom_classloader("jar/wurcsframework-" + @@v1216 + ".jar")
+      @wfw_v1215_loader = self.create_custom_classloader("jar/wurcsframework-" + @@v1215 + ".jar")
       @validator_glytoucan = java.lang.Class.forName("org.glycoinfo.WURCSFramework.util.validation.WURCSValidator",true,@wfw_glytoucan_loader)
-      @validator_131 = java.lang.Class.forName("org.glycoinfo.WURCSFramework.util.validation.WURCSValidator", true, @wfw_131_loader)
+      @validator_v131 = java.lang.Class.forName("org.glycoinfo.WURCSFramework.util.validation.WURCSValidator", true, @wfw_v131_loader)
+      @validator_v130 = java.lang.Class.forName("org.glycoinfo.WURCSFramework.util.validation.WURCSValidator", true, @wfw_v130_loader)
+      @validator_v1216 = java.lang.Class.forName("org.glycoinfo.WURCSFramework.util.validation.WURCSValidator", true, @wfw_v1216_loader)
+      @validator_v1215 = java.lang.Class.forName("org.glycoinfo.WURCSFramework.util.validation.WURCSValidator", true, @wfw_v1215_loader)
   end
 
   def create_custom_classloader(jar_path)
@@ -23,34 +36,59 @@ class WurcsVerify
     URLClassLoader.newInstance([url].to_java(URL), java.lang.Thread.currentThread.getContextClassLoader)
   end
 
-  def validatorVerify(w)
+  def validatorVerify(w,style)
     v_glytoucan = @validator_glytoucan.new_instance
     v_glytoucan.start(w)
 
-    v_131 = @validator_131.new_instance
+    v_131 = @validator_v131.new_instance
     v_131.start(w)
 
-    ret = {"1.2.14" => self.dovalidator(v_glytoucan),
-            "1.3.1" => self.dovalidator131(v_131) }
+    v_130 = @validator_v130.new_instance
+    v_130.start(w)
+
+    v_1216 = @validator_v1216.new_instance
+    v_1216.start(w)
+
+    v_1215 = @validator_v1215.new_instance
+    v_1215.start(w)
+
+    if (style == "v3")
+      ret = {@@gtc_version => self.dovalidatorv3(v_glytoucan,@@gtc_version),
+        @@v131  => self.dovalidatorv3(v_131, @@v131),
+        @@v130  => self.dovalidatorv3(v_130, @@v130),
+        @@v1215 => self.dovalidatorv3(v_1215, @@v1215),
+        @@v1216 => self.dovalidatorv3(v_1216, @@v1216)
+      }
+    elsif (style == "wfw")
+      ret = {@@gtc_version => self.dovalidator(v_glytoucan,@@gtc_version),
+      @@v131  => self.dovalidator(v_131, @@v131),
+      @@v130  => self.dovalidator(v_130, @@v130),
+      @@v1215 => self.dovalidator(v_1215, @@v1215),
+      @@v1216 => self.dovalidator(v_1216, @@v1216)
+      }
+    else
+      pp "strange second paramerters"
+    end
     return ret
   end
 
-  def dovalidator(validator)
-    reports = { "VALIDATOR" => ["WURCSFramework-1.2.14"],
+  def dovalidatorv3(validator,version)
+    if validator.getReport().respond_to?(:isStandardized)
+      flag = validator.getReport().isStandardized()
+    else
+      flag = "undef"
+    end
+    reports = { "VALIDATOR" => ["WURCSFramework-" + version],
                 "WARNING" => validator.getReport().hasWarning(),
+                "FLAG" => flag,
                 "ERROR" => validator.getReport().hasError(),
                 "UNVERIFIABLE" => validator.getReport().hasUnverifiable() }
     return {"message" => reports,
             "StandardWURCS" => validator.getReport().standard_string(),
             "RESULTS" => validator.getReport().getResults() }
   end
-  def dovalidator131(validator)
-    reports = { "VALIDATOR" => ["WURCSFramework-1.3.1"],
-                "WARNING" => validator.getReport().hasWarning(),
-                "ERROR" => validator.getReport().hasError(),
-                "UNVERIFIABLE" => false }
-    return {"message" => reports,
-            "StandardWURCS" => validator.getReport().standard_string(),
-            "RESULTS" => validator.getReport().getResults() }
+  def dovalidator(validator,version)
+    return {"VALIDATOR" => ["WURCSFramework-" + version],
+            "Report" => validator.getReport().getResults()}
   end
 end
---
+++ sample2.rb
@@ -0,0 +1,9 @@
+
+
+w = "WURCS=2.0/3,5,4/[a2122h-1b_1-5_2*NCC/3=O][a1122h-1b_1-5][a1122h-1a_1-5]/1-1-2-3-3/a4-b1_b4-c1_c3-d1_c6-e1"
+
+require 'wurcsverify'
+
+wf = WurcsVerify.new
+
+pp wf.validatorVerify(w,"v3")
